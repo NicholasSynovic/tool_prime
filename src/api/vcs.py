9c31bde05cafcb3bdff5ac5928626aeb3c544b9b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-05-10 17:55:29 -0500

    Implement Git VCS class

--- src/api/vcs.py
+++ src/api/vcs.py
@@ -1,6 +1,11 @@
+import sys
 from abc import ABC, abstractmethod
+from os import getcwd
 from pathlib import Path
 from typing import Any, List
+
+from git import Commit, Repo
+from git.exc import InvalidGitRepositoryError
 
 
 class VersionControlSystem(ABC):
@@ -16,8 +21,8 @@ class VersionControlSystem(ABC):
         Args:
             repo_path (Path): Path to the repository.
         """
-        self.repo_path = repo_path
-        self.repo: Any = self._initialize_repo()
+        self.repo_path: Path = repo_path
+        self.repo: Repo | Any = self._initialize_repo()
 
     @abstractmethod
     def _initialize_repo() -> Any:
@@ -28,9 +33,28 @@ class VersionControlSystem(ABC):
         pass
 
     @abstractmethod
-    def get_revisions(self) -> List[str]:
+    def get_revisions(self) -> List[Any]:
         """
         Abstract method to retrieve a list of revisions.
         Subclasses must implement this method.
         """
         pass
+
+
+class Git(VersionControlSystem):
+    def __init__(self, repo_path: Path):
+        super().__init__(repo_path=repo_path)
+
+    def _initialize_repo(self) -> Repo:
+        try:
+            return Repo(path=self.repo_path)
+        except InvalidGitRepositoryError:
+            sys.exit(1)
+
+    def get_revisions(self) -> List[Commit]:
+        return [commit for commit in self.repo.iter_commits()]
+
+
+if __name__ == "__main__":
+    git: Git = Git(repo_path=getcwd())
+    print(git.get_revisions())
