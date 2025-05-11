8de21ae6856a78566cc9da3b8c2f968918e3716c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-05-11 11:44:48 -0500

    Implement method to parse Git commits for data

--- src/api/vcs.py
+++ src/api/vcs.py
@@ -2,10 +2,12 @@ import sys
 from abc import ABC, abstractmethod
 from os import getcwd
 from pathlib import Path
-from typing import Any, List
+from typing import Any, Iterator, List, Tuple
 
 from git import Commit, Repo
 from git.exc import InvalidGitRepositoryError
+from pandas import DataFrame
+from progress.bar import Bar
 
 
 class VersionControlSystem(ABC):
@@ -23,6 +25,7 @@ class VersionControlSystem(ABC):
         """
         self.repo_path: Path = repo_path
         self.repo: Repo | Any = self._initialize_repo()
+        self.parseRevisionsBarMessage: str = "Parsing revisions..."
 
     @abstractmethod
     def _initialize_repo() -> Any:
@@ -33,9 +36,17 @@ class VersionControlSystem(ABC):
         pass
 
     @abstractmethod
-    def get_revisions(self) -> List[Any]:
+    def get_revisions(self) -> Tuple[Any, int]:
         """
-        Abstract method to retrieve a list of revisions.
+        Abstract method to retrieve revisions and the number of revisions.
+        Subclasses must implement this method.
+        """
+        pass
+
+    @abstractmethod
+    def parse_revisions(self, revisions: Any) -> DataFrame:
+        """
+        Abstract method to parse a List[Revisions] and extract relevant data
         Subclasses must implement this method.
         """
         pass
@@ -51,10 +62,58 @@ class Git(VersionControlSystem):
         except InvalidGitRepositoryError:
             sys.exit(1)
 
-    def get_revisions(self) -> List[Commit]:
-        return [commit for commit in self.repo.iter_commits()]
+    def get_revisions(self) -> Tuple[Iterator[Commit], int]:
+        revisionCount: int = sum(1 for _ in self.repo.iter_commits())
+        return (self.repo.iter_commits(), revisionCount)
+
+    def parse_revisions(
+        self,
+        revisions: Tuple[Iterator[Commit], int],
+    ) -> DataFrame:
+        data: List[dict] = []
+
+        with Bar(self.parseRevisionsBarMessage, max=revisions[1]) as bar:
+            commit: Commit
+            for commit in revisions[0]:
+                data.append(
+                    {
+                        "author": commit.author.name,
+                        "author_email": commit.author.email,
+                        "author_tz_offset": commit.author_tz_offset,
+                        "authored_datetime": commit.authored_datetime,
+                        "co_authors": [
+                            co_author.name for co_author in commit.co_authors
+                        ],
+                        "co_authors_email": [
+                            co_author.email for co_author in commit.co_authors
+                        ],
+                        "commit_hash": commit.hexsha,
+                        "committed_datetime": commit.committed_datetime,
+                        "committer": commit.committer.name,
+                        "committer_email": commit.committer.email,
+                        "committer_tz_offset": commit.committer_tz_offset,
+                        "encoding": commit.encoding,
+                        "gpgsign": commit.gpgsig,
+                        "message": commit.message,
+                        "parents": [
+                            parent.hexsha for parent in commit.parents
+                        ],  # noqa: E501
+                    }
+                )
+                bar.next()
+
+        return DataFrame(data=data)
 
 
 if __name__ == "__main__":
     git: Git = Git(repo_path=getcwd())
-    print(git.get_revisions())
+    revisions: Iterator[Commit] = git.get_revisions()
+
+    df: DataFrame = git.parse_revisions(revisions=revisions)
+
+    df.to_json(
+        path_or_buf="test.json",
+        indent=4,
+        index=False,
+        orient="records",
+    )
