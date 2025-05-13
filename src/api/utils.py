63eb6ca985316b904b1de83b1eaedd30d4a71095
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-05-13 09:59:13 -0500

    Migrate vcs parsing code from main.py to vcs.py

---
+++ src/api/utils.py
@@ -0,0 +1,96 @@
+from json import dumps
+from typing import Any, List
+
+from pandas import DataFrame, Series
+from progress.bar import Bar
+
+__all__ = [
+    "copyDFColumnsToDF",
+    "removeDuplicateDFRows",
+    "replaceDFValueInColumnWithIndexReference",
+    "copyDFColumnsAndRemoveDuplicateRowsByColumn",
+    "replaceDFValueInColumnWithListOfIndexReferences",
+]
+
+
+def copyDFColumnsToDF(df: DataFrame, columns: List[str]) -> DataFrame:
+    return df[columns].copy()
+
+
+def removeDuplicateDFRows(df: DataFrame, column: str) -> None:
+    """
+    Keeps the first instance in a column.
+    Edits `df` inplace.
+    Replaces the index after dropping duplicate rows.
+    """
+    df.drop_duplicates(
+        subset=column,
+        keep="first",
+        inplace=True,
+        ignore_index=True,
+    )
+
+
+def copyDFColumnsAndRemoveDuplicateRowsByColumn(
+    df: DataFrame, columns: List[str], checkColumn: str
+) -> DataFrame:
+    dfCopy: DataFrame = copyDFColumnsToDF(df=df, columns=columns)
+    removeDuplicateDFRows(df=dfCopy, column=checkColumn)
+    return dfCopy
+
+
+def replaceDFValueInColumnWithIndexReference(
+    df1: DataFrame,
+    df2: DataFrame,
+    df1Col: str,
+    df2Col: str,
+) -> DataFrame:
+    """
+    Replaces the values in DataFrame `df1` in column `df1Col` by the index of the values in DataFrame `df2` in column `df2Col`. # noqa: E501
+
+    Returns DataFrame `df1`
+    """
+    value_to_index = df2.reset_index().set_index(df2Col)["index"].to_dict()
+
+    df1 = df1.copy()
+    df1[df1Col] = df1[df1Col].map(value_to_index)
+    return df1
+
+
+def replaceDFValueInColumnWithListOfIndexReferences(
+    df1: DataFrame,
+    df2: DataFrame,
+    df1Col: str,
+    df2Col: str,
+) -> DataFrame:
+    """
+    Replace the values stored in a List in DataFrame `df1` in column `df1Col` by the index of the values in DataFrame `df2` in column `df2Col`. # noqa: E501
+
+    Returns DataFrame `df1`
+    """
+    value_to_index = df2.reset_index().set_index(df2Col)["index"].to_dict()
+    df1 = df1.copy()
+
+    with Bar(f"Updating values in ''{df1Col}''...", max=df1.shape[0]) as bar:
+        idx: int
+        row: Series
+        for idx, row in df1.iterrows():
+            value_list: List[Any] = row[df1Col]
+            new_list: List[dict[str, any]] = []
+
+            for value in value_list:
+                replacement_index = value_to_index.get(value)
+                new_list.append({df2Col: replacement_index})
+
+            df1.at[idx, df1Col] = new_list
+            bar.next()
+
+    df1[df1Col] = df1[df1Col].apply(
+        lambda x: (
+            [{df2Col: None}] if isinstance(x, list) and len(x) == 0 else x
+        )  # noqa: E501
+    )
+
+    df1[df1Col] = df1[df1Col].apply(lambda x: dumps(obj=x))
+
+    return df1
--- src/api/vcs.py
+++ src/api/vcs.py
@@ -1,6 +1,5 @@
-import sys
 from abc import ABC, abstractmethod
-from datetime import timezone
+from datetime import datetime, timezone
 from os import getcwd
 from pathlib import Path
 from typing import Any, Iterator, List, Tuple
@@ -9,6 +8,13 @@ from git import Commit, Repo
 from git.exc import InvalidGitRepositoryError
 from pandas import DataFrame
 from progress.bar import Bar
+
+from src.api.utils import (
+    copyDFColumnsAndRemoveDuplicateRowsByColumn,
+    copyDFColumnsToDF,
+    replaceDFValueInColumnWithIndexReference,
+    replaceDFValueInColumnWithListOfIndexReferences,
+)
 
 
 class VersionControlSystem(ABC):
@@ -54,15 +60,46 @@ class VersionControlSystem(ABC):
         pass
 
 
+class Revision:
+    def __init__(
+        self,
+        author: str,
+        authorEmail: str,
+        authoredDatetime: datetime,
+        coAuthors: List[str],
+        coAuthorEmails: List[str],
+        commitHash: str,
+        committedDatetime: datetime,
+        committer: str,
+        committerEmail: str,
+        encoding: str,
+        message: str,
+        gpgsign: str,
+        parents: List[str],
+    ):
+        self.data: dict[str, Any] = {
+            "author": author,
+            "author_email": authorEmail,
+            "authored_datetime": authoredDatetime,
+            "co_authors": coAuthors,
+            "co_author_emails": coAuthorEmails,
+            "commit_hash": commitHash,
+            "committed_datetime": committedDatetime,
+            "committer": committer,
+            "committer_email": committerEmail,
+            "encoding": encoding,
+            "gpgsign": gpgsign,
+            "message": message,
+            "parents": parents,
+        }
+
+
 class Git(VersionControlSystem):
     def __init__(self, repo_path: Path):
         super().__init__(repo_path=repo_path)
 
     def _initialize_repo(self) -> Repo:
-        try:
-            return Repo(path=self.repo_path)
-        except InvalidGitRepositoryError:
-            sys.exit(1)
+        return Repo(path=self.repo_path)
 
     def get_revisions(self) -> Tuple[Iterator[Commit], int]:
         revisionCount: int = sum(1 for _ in self.repo.iter_commits())
@@ -80,39 +117,127 @@ class Git(VersionControlSystem):
     ) -> DataFrame:
         data: List[dict] = []
 
-        with Bar(self.parseRevisionsBarMessage, max=revisions[1]) as bar:
+        with Bar(
+            self.parseRevisionsBarMessageList[DataFrame],
+            max=revisions[1],
+        ) as bar:
             commit: Commit
             for commit in revisions[0]:
                 data.append(
-                    {
-                        "author": commit.author.name,
-                        "author_email": commit.author.email,
-                        "authored_datetime": commit.authored_datetime.astimezone(  # noqa: E501
+                    Revision(
+                        author=commit.author.name,
+                        authorEmail=commit.author.email,
+                        authoredDatetime=commit.authored_datetime.astimezone(
                             tz=timezone.utc
                         ),
-                        "co_authors": [
+                        coAuthors=[
                             co_author.name for co_author in commit.co_authors
-                        ],
-                        "co_author_emails": [
+                        ],  # noqa: E501
+                        coAuthorEmails=[
                             co_author.email for co_author in commit.co_authors
                         ],
-                        "commit_hash": commit.hexsha,
-                        "committed_datetime": commit.committed_datetime.astimezone(  # noqa: E501
+                        commitHash=commit.hexsha,
+                        committedDatetime=commit.committed_datetime.astimezone(
                             tz=timezone.utc
                         ),
-                        "committer": commit.committer.name,
-                        "committer_email": commit.committer.email,
-                        "encoding": commit.encoding,
-                        "gpgsign": commit.gpgsig,
-                        "message": commit.message,
-                        "parents": [
-                            parent.hexsha for parent in commit.parents
-                        ],  # noqa: E501
-                    }
+                        committer=commit.committer.name,
+                        committerEmail=commit.committer.email,
+                        encoding=commit.encoding,
+                        gpgsign=commit.gpgsig,
+                        message=commit.message,
+                        parents=[parent.hexsha for parent in commit.parents],
+                    ).data
                 )
                 bar.next()
 
         return DataFrame(data=data)
+
+
+def identifyVCS(repoPath: Path) -> VersionControlSystem | int:
+    try:
+        return Git(repo_path=repoPath)
+    except InvalidGitRepositoryError:
+        return -1
+
+
+def parseVCS(vcs: VersionControlSystem) -> dict[str, DataFrame]:
+    data: dict[str:DataFrame] = {}
+
+    # Extract the commit log
+    revisions: Tuple[Any, int] = vcs.get_revisions()
+    commitLogDF: DataFrame = vcs.parse_revisions(revisions=revisions)
+
+    # Copy static information to output data structure
+    data["commit_hashes"] = copyDFColumnsToDF(
+        df=commitLogDF,
+        columns=["commit_hash"],
+    )
+    data["authors"] = copyDFColumnsAndRemoveDuplicateRowsByColumn(
+        df=commitLogDF,
+        columns=["author", "author_email"],
+        checkColumn="author_email",
+    )
+    data["committers"] = copyDFColumnsAndRemoveDuplicateRowsByColumn(
+        df=commitLogDF,
+        columns=["committer", "committer_email"],
+        checkColumn="committer_email",
+    )
+
+    # Replace commit log information with the index to static DataFrames
+    commitLogDF = replaceDFValueInColumnWithIndexReference(
+        df1=commitLogDF,
+        df2=data["commit_hashes"],
+        df1Col="commit_hash",
+        df2Col="commit_hash",
+    )
+    commitLogDF = replaceDFValueInColumnWithIndexReference(
+        df1=commitLogDF,
+        df2=data["authors"],
+        df1Col="author_email",
+        df2Col="author_email",
+    )
+    commitLogDF = replaceDFValueInColumnWithIndexReference(
+        df1=commitLogDF,
+        df2=data["committers"],
+        df1Col="committer_email",
+        df2Col="committer_email",
+    )
+
+    # Replace commit log information with a list of indicies from static
+    # DataFrames
+    commitLogDF = replaceDFValueInColumnWithListOfIndexReferences(
+        df1=commitLogDF,
+        df2=data["authors"],
+        df1Col="co_author_emails",
+        df2Col="author_email",
+    )
+    commitLogDF = replaceDFValueInColumnWithListOfIndexReferences(
+        df1=df,
+        df2=data["commit_hashes"],
+        df1Col="parents",
+        df2Col="commit_hash",
+    )
+
+    # Drop irrelevant columns and rename existing columns to match database
+    # schema
+    commitLogDF.drop(
+        columns=["author", "committer", "co_authors"],
+        inplace=True,
+    )
+    commitLogDF.rename(
+        columns={
+            "author_email": "author_id",
+            "co_author_emails": "co_author_ids",
+            "commit_hash": "commit_hash_id",
+            "committer_email": "committer_id",
+            "parents": "parent_hash_ids",
+        },
+        inplace=True,
+    )
+
+    data["commit_logs"] = commitLogDF
+
+    return data
 
 
 if __name__ == "__main__":
