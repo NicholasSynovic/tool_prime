ba98e3a878deb28876ff849479b1cc504ff77ec4
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-06-05 08:59:47 -0500

    Add docstrings

--- src/api/size.py
+++ src/api/size.py
@@ -1,3 +1,10 @@
+"""
+SCC executor.
+
+Copyright (C) 2025 Nicholas M. Synovic.
+
+"""
+
 from pandas import DataFrame
 import pandas as pd
 import subprocess  # nosec
@@ -8,7 +15,30 @@ import shlex
 
 
 class SCC:
-    def __init__(self, directory: Path):
+    """
+    Class to execute SCC.
+
+    This class defines the structure and required methods for interacting with
+    SCC.
+
+    Attributes:
+        directory (Path): The file system path to the repository.
+        command (str): SCC program name.
+        options (str): SCC options.
+
+    Methods:
+        run(): Run SCC in a particular directory.
+
+    """
+
+    def __init__(self, directory: Path) -> None:
+        """
+        Initialize the SCC object with a specified directory.
+
+        Args:
+            directory (Path): The directory to use for the SCC command.
+
+        """
         self.directory: Path = directory.resolve()
         self.command: str = "scc"
         self.options: str = (
@@ -16,10 +46,25 @@ class SCC:
         )
 
     def run(self) -> DataFrame:
+        """
+        Execute the SCC and parse the output as a CSV DataFrame.
+
+        This method executes the specified command and parses the output as a pandas
+        DataFrame.
+
+        Returns:
+            DataFrame: A pandas DataFrame representing the output.
+
+        """
+        args: list[str] = (
+            [self.command] + shlex.split(s=self.options) + [str(self.directory)]  # noqa: RUF005
+        )
+
         result: CompletedProcess = subprocess.run(  # nosec
-            args=[self.command] + shlex.split(s=self.options) + [str(self.directory)],
+            args=args,
             capture_output=True,
             text=True,
+            check=False,
         )
 
         return pd.read_csv(filepath_or_buffer=StringIO(result.stdout))
