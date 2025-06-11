6aa08bf6281a8fa355ed378ffb545a5839fa548a
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-06-11 11:16:33 -0500

    Add function to get the minimum set of issue information

--- src/api/issues.py
+++ src/api/issues.py
@@ -2,6 +2,7 @@ from requests import Response, post
 from pandas import DataFrame
 import pandas as pd
 from string import Template
+from datetime import datetime
 
 
 def query_graphql(
@@ -24,7 +25,20 @@ class GitHubIssues:
 
     def get_total_issues(self) -> int:
         json_template: Template = Template(
-            template='query { repository(name:"$name", owner:"$owner"){issues {totalCount}}, rateLimit {limit, cost, remaining, resetAt}}'  # noqa: E501
+            template="""
+            query {
+                repository(name:"$name", owner:"$owner") {
+                    issues {
+                        totalCount
+                    }
+                },
+                rateLimit {
+                    limit,
+                    cost,
+                    remaining,
+                    resetAt
+                }
+            }"""
         )
         json_query = json_template.substitute(name=self.repo_name, owner=self.owner)
 
@@ -42,10 +56,63 @@ class GitHubIssues:
 
         return response.json()["data"]["repository"]["issues"]["totalCount"]
 
-    def get_issues(self) -> DataFrame:
-        data: list[DataFrame] = []
-        return pd.concat(objs=data, ignore_index=True)
+    def get_issues(self, cursor: str = "null") -> tuple[DataFrame, str, bool]:
+        json_template: Template = Template(
+            template="""query {
+                repository(name:"$name", owner:"$owner") {
+                    issues(first: 100, after: $cursor) {
+                        edges {
+                            node {
+                                id,
+                                createdAt,
+                                closedAt,
+                            }
+                        },
+                        pageInfo {
+                            endCursor,
+                            hasNextPage
+                        }
+                    }
+                },
+                rateLimit {
+                    limit,
+                    cost,
+                    remaining,
+                    resetAt
+                }
+            }"""
+        )
+
+        json_query = json_template.substitute(
+            name=self.repo_name, owner=self.owner, cursor="null"
+        )
+
+        headers: dict[str, str] = {
+            "Authorization": f"Bearer {self.auth_key}",
+            "Content-Type": "application/json",
+        }
+
+        response: Response = query_graphql(
+            url=self.base_url, json_query=json_query, headers=headers
+        )
+
+        if response.status_code != 200:
+            return (DataFrame(), "", False)
+
+        pageInfo: dict[str, str | bool] = response.json()["data"]["repository"][
+            "issues"
+        ]["pageInfo"]
+
+        cursor: str = str(pageInfo["endCursor"])
+        hasNextPage: bool = bool(pageInfo["hasNextPage"])
+
+        nodes: list[dict[str, dict[str, str]]] = response.json()["data"]["repository"][
+            "issues"
+        ]["edges"]
+
+        return (DataFrame(data=map(lambda x: x["node"], nodes)), cursor, hasNextPage)
 
 
 ghi: GitHubIssues = GitHubIssues(owner="numpy", repo_name="numpy", auth_key="test")
-ghi.get_total_issues()
+# ghi.get_total_issues()
+print(ghi.get_issues())
